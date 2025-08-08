86674103f47cbe08e2bf2daf7100401fa5328fc7
Dom Rodriguez <shymega@shymega.org.uk>
2025-08-08 07:58:17 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = inputs: let
+    inherit (inputs) self nixpkgs;
+    forEachSystem = let
+      inherit (nixpkgs.lib) genAttrs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+      ];
+    in
+      f: genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    packages = forEachSystem (pkgs: {
+      flake-locker = pkgs.callPackage ./package.nix {inherit self;};
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.flake-locker;
+    });
+  };
+}
---
+++ src/flake_locker/__main__.py
@@ -0,0 +1,10 @@
+import json
+import argparse
+import logging
+import os
+import pathlib
+import subprocess
+
+
+if __name__ == "__main__":
+    main()
